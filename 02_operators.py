a3b2f833c116aa0a17e9c47b63a030853db5ffc1
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-04-16 19:16:20 -0300

    Se ven todos los operadores logicos, comparativos y aritmeticos

---
+++ 02_operators.py
@@ -0,0 +1,38 @@
+# Operadores
+
+#Print : imprime en la consola lo que le pasamos por parametro.
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print( 2 ** 3 + 9 -7 / 1 // 4)
+
+# El operador % es el residuo de una division, o sea nos devuelve lo que sobra despues de hacer una division entera.
+print(10 % 2)
+
+# El operador //:  devuelve el cociente de una division, es decir, da los enteros completos, si da como resultado decimal, lo aproxima a un número entero.
+print(10 // 3)
+
+# El operador **:  eleva el numero a una potencia.
+print(2 ** 3)
+
+print("Hola" + "Python" + "¿Qué tal?")
+
+# Operadores comparativos
+
+print (3 > 4)
+print (3 < 4)
+print (3 >= 4)
+print (3 == 4)
+print (3 != 4)
+
+
+# Operadores logicos
+# and, or y not son los operadores logicos de python
+
+print( 3 > 4 and "Hola" > "Python")
+print( 3 > 4 or "Hola" > "Python")
+print( 3 < 4 or "Hola" < "Python")
+print( 3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not(3 > 4))
