35dd214097a9327c4d900a424c6052701cf44034
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-04-15 22:18:50 -0300

    Se explica los tipos de datos en Python

---
+++ hello.py
@@ -0,0 +1,17 @@
+# Esto es un comentario: 
+# Mi Hola mundo en python
+print("Hola Python")
+
+"""
+Este es un comentario
+en varias
+lineas.
+"""
+
+# TIPOS DE DATOS EN PYTHON
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(7)) # Tipo 'int'
+print(type(7.7)) # Tipo 'float'
+print(type(True)) # Tipo 'bool'
