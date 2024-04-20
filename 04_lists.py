30caeb1a047cee84f2f90ad58aaa46a2f7570dd1
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-04-20 10:34:27 -0300

    Se ven algunas funciones que se pueden ejecutar con las listas, además de diferentes metodos puestos en ejemplo mediante un contexto de la vida cotidiana

--- 04_lists.py
+++ 04_lists.py
@@ -22,4 +22,54 @@ print(my_other_list[-3])
 print(my_list.count(30))
 
 age, height, name, surname = my_other_list
-print(name)
+print(age)
+
+print(my_list + my_other_list)
+
+
+# .append sirve  para agregar un elemento al final de la lista
+my_other_list.append("Jimenez")
+print(my_other_list)
+
+#  .insert sirve para  insertar un elemento en una posicion especifica de la lista
+my_other_list.insert(1, "Rojo")
+print(my_other_list)
+
+my_other_list[1] = "Azul"
+print(my_other_list)
+
+#  .remove sirve para  eliminar un elemento de la lista
+my_other_list.remove("Azul")
+print(my_other_list)
+
+#   .pop se utiliza para sacar el ultimo o el que esta en una posicion especificada
+my_list.pop()
+print(my_list)
+
+my_pop_element = my_list.pop(2)
+print(my_pop_element)
+print(my_list)
+
+#  "del" sirve para  borrar una variable completa o en este caso un elemento de la lista
+del my_list[2]
+print(my_list)
+
+# .copy  crea una copia exacta de la lista
+my_new_list = my_list.copy()
+
+# .clear  vacia completamente la lista 
+my_list.clear()
+print(my_list)
+print(my_new_list)
+
+# .reverse invierte los elementos de la lista
+my_new_list.reverse()
+print(my_new_list)
+
+#  .sort ordena los elementos de la lista
+my_new_list.sort()
+print(my_new_list)
+
+my_list = "Hola Python"
+print(my_list)
+print(type(my_list))
