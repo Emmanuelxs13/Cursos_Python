55403cc18bb66c1fabd630114e7402ae06d8c695
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-05-21 19:46:05 -0500

    Se crea carpeta para la creación de ejercios o notebooks para el certificado de python

---
+++ Ejercicios_Certificado/notebook_diccionarios.py
@@ -0,0 +1,11 @@
+#SI BIEN EN ANALITICA NOS PODEMOS ENCONTRAR CON CUALQUIER TIPO DE ESTRUCTURA DE DATOS (DATA FRAME), CONOCER Y ENTENDER LOS
+#DICCIONARIOS, PROVEE ELEMENTOS DE GRAN IMPORTANCIA EN EL USO DE PYTHON CON LA ANALÍTICA.
+
+#Los diccionarios en Python nos permiten almacenar una serie de 'mapeos' entre dos conjuntos de elementos, llamados keys and values 
+# (Claves y Valores). Todos los elementos en el diccionario se encuentran encerrados en un par de corchetes 
+
+#Asumamos que tenemos una lista e personas con sus edades, la podemos declarar así:
+dicc = {"Ana": 20,"Bruno": 14,"Cata": 19,"Dani": 7,"Esteban": 12,"Fede": 5}
+
+#La visualizamos en consola así:
+print(dicc)
