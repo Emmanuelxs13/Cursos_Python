8d995043d70370a84047ad0c77c7dea321a86f69
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-05-23 20:21:51 -0500

    Se crea el archivo para el notebook de clasificación y sus respectivos ejercicios de practica

---
+++ Ejercicios_Certificado/notebook_clasificacion.py
@@ -0,0 +1,3 @@
+#CLASIFICACIÓN
+#En aprendizaje automático y estadística, la clasificación estadística es el problema de identificar a cuál de un subconjunto de categorías pertenece una nueva observación, sobre la base de un conjunto de datos de formación que contiene observaciones cuya categoría de miembros es conocida. 
+#Un ejemplo sería asignar a un correo electrónico dado la clasificación de "spam" o "no spam".
