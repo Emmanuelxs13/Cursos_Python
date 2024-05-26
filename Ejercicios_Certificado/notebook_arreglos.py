95f2b3f00c02e68eb163ec5ab2a6ff84b87bd737
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-05-26 11:14:01 -0500

    Se realiza un ejercicio basico para entender el funcionamiento de los Arrays

--- Ejercicios_Certificado/notebook_arreglos.py
+++ Ejercicios_Certificado/notebook_arreglos.py
@@ -1,1 +1,24 @@
 # Hagamos algunos calculos con arreglos que serán de gran utulidad para entender y usar el modelado
+
+#Declaremos dos variables del tipo texto
+
+primero = "papas"
+segundo = "fritas"
+
+#Ahora pidamos el tamaño de las variables con el método 'len'
+
+print(len(primero))
+print(len(segundo))
+
+# podemos sumar ambas variables y obtener un total así:
+print(len(primero)+len(segundo))
+
+#Ahora inténtelo ud mismo:
+#Suponga que tiene dos variables 'Soy el mas', 'grande' y quiere obtener el tamaño (longitud) de estas dos variables juntas.
+#Debe obtener el siguiente valor:
+# 16
+
+var_uno = "Soy el más"
+var_dos = "grande"
+
+print(len(var_uno) + len(var_dos))
