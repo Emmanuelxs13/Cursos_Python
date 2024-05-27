5830fa8798603d28dffd221b545e2538b0291a2c
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-05-27 09:36:19 -0500

    Se aprende la teoria sobre operaciones con arrays

--- Ejercicios_Certificado/Operaciones_arrays.py
+++ Ejercicios_Certificado/Operaciones_arrays.py
@@ -2,3 +2,15 @@
 
 #Por ejemplo si queremos tener la suma de un arreglo como este:
 arreglo1 = [1,2,3]
+
+#Sería simplemente sumar los tres elementos para obtener un valor igual a 6, 
+#sin emabrago, en un arreglo donde seguramente tenedremos muchos más elmentos, necesitaremos la ayuda del loop for así:
+
+#Se inicializa la suma:
+s=0
+#Creamos el for para iterar con la suma de cada término
+for suma1 in arreglo1:
+    s=suma1+s
+#Mostramos el resultado obtenido
+print(s)
+
