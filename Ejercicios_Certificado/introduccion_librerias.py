03d4b7a17c26fc6acb86b0d9693ce42e01fcec6a
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-05-30 15:28:02 -0500

    Se crea archivo para la introducción con las librerias de python

---
+++ Ejercicios_Certificado/introduccion_librerias.py
@@ -0,0 +1,7 @@
+# INTRODUCCIÓN A LIBRERIAS Y FUNCIONES
+
+# Hay distintas metodologías que simplifican enormemente el uso de código en cualquier lenguaje de programación.
+
+# Así, para el uso de ciertas herramientas están sijetas a uso de 'librerias. Este concepto hace referencia al conjunto de 
+#implementos funcionales que ayudan a codificar todo este lenguaje de programación para crear una interfaz independiente y simplificar
+#el uso con fines específicos.
