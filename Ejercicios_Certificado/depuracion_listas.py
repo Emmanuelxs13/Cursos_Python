44ad473635eed73fa0a84b1506fc3fc807a28186
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-05-30 15:02:51 -0500

    Se realiza un ejercicio practico para entneder el método list dict.fromkeys

--- Ejercicios_Certificado/depuracion_listas.py
+++ Ejercicios_Certificado/depuracion_listas.py
@@ -10,3 +10,14 @@ print(lista)
 # usamos el método list dict.fromkeys, para eliminar los valores que se presentan más de una vez
 lista_depurada = list(dict.fromkeys(lista))
 print(lista_depurada)
+
+# Ahora inténtelo ud mismo.
+# Suponga que tiene la siguiente lista y quiere depurar para eliminar los valores que aparecen más de una vez.
+# ["peras", "bananas", "manzanas", "bananas", "uvas", "manzanas"]
+
+#Debe obtener el siguiente resultado:
+# ['peras', 'bananas', 'manzanas', 'uvas']
+lista2 = ["peras", "bananas", "manzanas", "bananas", "uvas", "manzanas"]
+lista_depurada2 = list(dict.fromkeys(lista2))
+
+print(lista_depurada2)
