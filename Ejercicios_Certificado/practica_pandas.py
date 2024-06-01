171ce96894da14708f1c7bec17a6353f75597065
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-05-31 22:26:50 -0500

    Se crea archivo para aprender a utilizar la libreria de PANDAS

---
+++ Ejercicios_Certificado/practica_pandas.py
@@ -0,0 +1,12 @@
+# PANDAS
+
+"Usada para la manipulación de marcos de datos (dataframe) y analizar datos"
+
+
+data = {
+  "ID_producto": [1421, 1230, 9080],
+  "ventas": [500, 40, 4550]
+}
+
+df = pd.DataFrame(data)
+
