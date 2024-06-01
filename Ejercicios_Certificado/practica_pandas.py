0cde7933bd80dd170f03975d1e816d0ef439d6e0
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-06-01 09:28:40 -0500

    Se ven algunos ejemplos con la libreria de PANDAS

--- Ejercicios_Certificado/practica_pandas.py
+++ Ejercicios_Certificado/practica_pandas.py
@@ -2,6 +2,7 @@
 
 "Usada para la manipulación de marcos de datos (dataframe) y analizar datos"
 
+import pandas as pd
 
 data = {
   "ID_producto": [1421, 1230, 9080],
@@ -10,3 +11,30 @@ data = {
 
 df = pd.DataFrame(data)
 
+print(df) 
+
+data = {
+  "ID_producto": [1421,None, 9080],
+  "ventas": [500, 40, 4550]
+}
+
+df = pd.DataFrame(data)
+
+print(df) 
+
+df.dropna(inplace = True)
+
+print(df) 
+
+data = {
+  "ID_producto": [1421,None, 9080],
+  "ventas": [500, 40, 4550]
+}
+
+df = pd.DataFrame(data)
+
+print(df) 
+
+df.fillna(130, inplace = True)
+
+print(df) 
