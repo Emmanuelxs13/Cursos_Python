22c5fe58320c162e014834424411daa59dfa0be2
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-05-31 21:29:59 -0500

    Se aprende la teoria y se brinda algunos ejemplos

--- Ejercicios_Certificado/uso_librerias.py
+++ Ejercicios_Certificado/uso_librerias.py
@@ -1,3 +1,23 @@
 #Uso de librerías:
 
 import time
+
+print (dir(time))
+
+time.time_ns()
+
+import time as tm
+
+tm.time_ns()
+
+from time import time_ns as tns
+
+a = tns()
+
+tns()
+
+import sklearn
+
+from sklearn.datasets import load_iris
+
+display(load_iris())
