6431dd0dc0b9d25d3d9e49cf93f987be5a5388fe
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-06-02 18:13:33 -0500

    Se realizan ejercicios practicos con NumPy

--- Ejercicios_Certificado/arreglos_numpy.py
+++ Ejercicios_Certificado/arreglos_numpy.py
@@ -1,1 +1,15 @@
 # Numpy Array
+
+import numpy as np
+
+arr = np.arrar([1, 2, 3, 4, 5])
+
+print(arr)
+print(type(arr))
+print(arr[0])
+print(arr[2] + print[3])
+
+arr2 = np.array([1, 2, 3, 4, 5, 4, 4])
+
+x = np.where(arr2 == 4)
+print(x)
