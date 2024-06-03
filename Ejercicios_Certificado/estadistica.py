fa99c1e32ff3ca03dff6aef81138e7498d18a307
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-06-03 10:08:36 -0500

    Se crea archivo para aprender sobre estadistica

---
+++ Ejercicios_Certificado/estadistica.py
@@ -0,0 +1,2 @@
+#La estadística es un "universo" de conocimiento tan basto que está fuera del alcance de deste curso, sin embargo,
+#existen conceptos básicos que ayudarán a iniciarse en este interesante tema:
