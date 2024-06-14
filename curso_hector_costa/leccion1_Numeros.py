fe9f8083eca87375b659dd6c3189439d2d6831ea
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-06-14 09:50:43 -0500

    Lwccion 1, numeros y variables

---
+++ curso_hector_costa/leccion1_Numeros.py
@@ -0,0 +1,45 @@
+# Estos ejercicios son desarrollados en Jupyter
+3+2
+3-2 
+3*2
+
+# División
+3/2
+
+# Modulo
+3%2
+
+# Potencia
+3**2
+
+3-2+4*10
+
+
+# Asignación de un valor a una variable
+n = 3
+n
+
+n+3
+
+n*2
+
+n*n
+
+m = 10
+
+n+m
+
+n*m+10
+
+n = 10
+m = 15
+n + m
+
+n = m
+n
+m
+
+n = m+10
+
+n=n+25
+n
