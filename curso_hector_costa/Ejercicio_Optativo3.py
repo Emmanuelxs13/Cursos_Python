977b56c834b296b7865127c119f218a3205c6e5c
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-07-30 21:21:34 -0500

    Se realiza ejercicio optativo numero 3 del curso

---
+++ curso_hector_costa/Ejercicio_Optativo3.py
@@ -0,0 +1,9 @@
+# El siguiente codigo pretende realizar una media entre 3 números, pero no funciona correctamente ¿Puedes identificare el error?
+ 
+numero_1 = 9
+numero_2 = 3
+numero_3 = 6
+
+media = (numero_1 + numero_2 + numero_3) / 3
+
+print("La media es: ", media)
