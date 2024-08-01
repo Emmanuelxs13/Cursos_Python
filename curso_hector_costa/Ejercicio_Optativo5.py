b7134a81046911b50bf16d97940a75900fc8ed8a
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-07-31 21:22:21 -0500

    Se escribe el problema a solucionar

---
+++ curso_hector_costa/Ejercicio_Optativo5.py
@@ -0,0 +1,9 @@
+# La siguiente matriz(o lista con listas anidadas) debe cumplir una condición, y es que en
+# cada fila, el cuarto elemennto siempre debe ser el resultado de sumar los tres primeros.
+
+matriz = [
+    [1, 1, 1, 3],
+    [2, 2, 2, 7],
+    [3, 3, 3, 9],
+    [4, 4, 4, 13]
+]
